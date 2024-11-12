cd13081a9cd32f0d3fa58c17b6d78210d35701a9
Reyhaneh Davaee <reyhanehdavaee@Reyhanehs-MacBook-Air.local>
2024-11-12 10:11:01 +0330

    part 7

---
+++ mysite/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("welcome page")
+
+
+urlpatterns = [
+    path("", welcome),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
+
---
+++ polls/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+
+# customizing how the admin form looks and works by following this pattern:
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+
+
+# 1 - creating a ModelAdmin class
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes":["colapse"]}),
+        # the first element of each tuple is the title of the fieldset.
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    
+
+    # this tells django that Choice objects are edited on the Question page.
+
+
+
+# 2 - pass the class the second argument to admin.site.register()
+admin.site.register(Question, QuestionAdmin)
+
+
+# 3 - these changes indicate how the admin models are displayed.
